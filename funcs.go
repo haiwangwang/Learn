de8543738ac6d737ebf45bf4d58b30c9a042f080
Mr wang <38740521@qq.com>
2021-11-12 16:40:19 +0800

    发布v2版本

--- funcs.go
+++ funcs.go
@@ -12,17 +12,17 @@ func GetNowTimestr() string {
 	return time.Unix(time.Now().Unix(),0).Format("2006-01-02 15:04:05")
 }
 
-func GetLocalIp() string {
+func GetLocalIp() (string,error) {
 	conn,err := net.Dial("udp","8.8.8.8:53")
 	if err != nil {
 		log.Printf("get local addr err:%v",err)
-		return ""
+		return "",err
 	}
 	localIp := strings.Split(conn.LocalAddr().String(),":")[0]
-	return localIp
+	return localIp,nil
 }
 
 func GetHostName()  string	{
 	name,_:= os.Hostname()
-	return name + "v1.1"
+	return name + "v2.0.1"
 }
